9ffb29f25e81ae003623ae6082968a95b0f561b0
Usama-Basharat-3622 <shopify.partner3622@gmail.com>
2025-08-25 02:19:11 +0500

    update account pages

--- assets/customer.js
+++ assets/customer.js
@@ -5,6 +5,8 @@ const selectors = {
   toggleAddressButton: 'button[aria-expanded]',
   cancelAddressButton: 'button[type="reset"]',
   deleteAddressButton: 'button[data-confirm-message]',
+  editAddressButton: '[data-edit-address-button]', 
+  cancelEditAddressButton: '[data-cancel-edit-address-button]'
 };
 
 const attributes = {
@@ -18,6 +20,22 @@ class CustomerAddresses {
     if (Object.keys(this.elements).length === 0) return;
     this._setupCountries();
     this._setupEventListeners();
+    this._setupCancelEditAddressButton();
+    this.previouslyFocusedElement = null;
+  }
+
+  _setupCancelEditAddressButton() {
+    const cancelEditAddressButtons = document.querySelectorAll(selectors.cancelEditAddressButton);
+    cancelEditAddressButtons.forEach((button) => {
+      button.addEventListener('click', (event) => {
+        event.preventDefault();
+        const editAddressFormContainer = document.querySelector(`.address-edit-form.active`);
+        editAddressFormContainer?.classList.remove('active');
+        if (this.previouslyFocusedElement) {
+          this.previouslyFocusedElement.focus();
+        }
+      });
+    });
   }
 
   _getElements() {
@@ -30,24 +48,25 @@ class CustomerAddresses {
           cancelButtons: container.querySelectorAll(selectors.cancelAddressButton),
           deleteButtons: container.querySelectorAll(selectors.deleteAddressButton),
           countrySelects: container.querySelectorAll(selectors.addressCountrySelect),
+          editButtons: container.querySelectorAll(selectors.editAddressButton),
         }
       : {};
   }
 
   _setupCountries() {
-    if (Shopify && Shopify.CountryProvinceSelector) {
-      // eslint-disable-next-line no-new
-      new Shopify.CountryProvinceSelector('AddressCountryNew', 'AddressProvinceNew', {
-        hideElement: 'AddressProvinceContainerNew',
-      });
-      this.elements.countrySelects.forEach((select) => {
-        const formId = select.dataset.formId;
-        // eslint-disable-next-line no-new
-        new Shopify.CountryProvinceSelector(`AddressCountry_${formId}`, `AddressProvince_${formId}`, {
-          hideElement: `AddressProvinceContainer_${formId}`,
-        });
-      });
-    }
+    // if (Shopify && Shopify.CountryProvinceSelector) {
+    //   // eslint-disable-next-line no-new
+    //   new Shopify.CountryProvinceSelector('AddressCountryNew', 'AddressProvinceNew', {
+    //     hideElement: 'AddressProvinceContainerNew',
+    //   });
+    //   this.elements.countrySelects.forEach((select) => {
+    //     const formId = select.dataset.formId;
+    //     // eslint-disable-next-line no-new
+    //     new Shopify.CountryProvinceSelector(`AddressCountry_${formId}`, `AddressProvince_${formId}`, {
+    //       hideElement: `AddressProvinceContainer_${formId}`,
+    //     });
+    //   });
+    // }
   }
 
   _setupEventListeners() {
@@ -60,10 +79,13 @@ class CustomerAddresses {
     this.elements.deleteButtons.forEach((element) => {
       element.addEventListener('click', this._handleDeleteButtonClick);
     });
+    this.elements.editButtons.forEach((element) => {
+      element.addEventListener('click', this.handleEditButtonClick.bind(this));
+    });
   }
 
   _toggleExpanded(target) {
-    target.setAttribute(attributes.expanded, (target.getAttribute(attributes.expanded) === 'false').toString());
+    target?.setAttribute(attributes.expanded, (target?.getAttribute(attributes.expanded) === 'false').toString());
   }
 
   _handleAddEditButtonClick = ({ currentTarget }) => {
@@ -71,15 +93,61 @@ class CustomerAddresses {
   };
 
   _handleCancelButtonClick = ({ currentTarget }) => {
-    this._toggleExpanded(currentTarget.closest(selectors.addressContainer).querySelector(`[${attributes.expanded}]`));
+    this._toggleExpanded(currentTarget?.closest(selectors.addressContainer)?.querySelector(`[${attributes.expanded}]`));
   };
 
   _handleDeleteButtonClick = ({ currentTarget }) => {
-    // eslint-disable-next-line no-alert
-    if (confirm(currentTarget.getAttribute(attributes.confirmMessage))) {
+    const confirmationModal = document.querySelector('.modal.delete-address-modal');
+    const body = document.querySelector('body');
+    const previouslyFocusedElement = document.activeElement; 
+
+    confirmationModal?.classList.add('open');
+    body?.classList.add('overflow-hidden');
+
+    trapFocus(confirmationModal, confirmationModal.querySelector('.close'));
+
+    const closeButton = confirmationModal.querySelector('.close');
+    closeButton?.addEventListener('click', () => {
+      confirmationModal?.classList.remove('open');
+      body?.classList.remove('overflow-hidden');
+
+      if (previouslyFocusedElement) {
+        previouslyFocusedElement.focus();
+      }
+    });
+
+    const confirmDeleteButton = confirmationModal.querySelector('.confirm-delete');
+    confirmDeleteButton?.addEventListener('click', () => {
       Shopify.postLink(currentTarget.dataset.target, {
         parameters: { _method: 'delete' },
       });
-    }
+      confirmationModal?.classList.remove('open');
+      body?.classList.remove('overflow-hidden');
+
+      if (previouslyFocusedElement) {
+        previouslyFocusedElement.focus();
+      }
+    });
   };
+
+  handleEditButtonClick(event) {
+    const addressId = event.currentTarget.getAttribute('data-address-id');    
+    const editAddressFormContainer = document.querySelector(`.customer-addresses-forms-list`); 
+    
+    editAddressFormContainer.querySelector('.address-edit-form.active')?.classList.remove('active');
+    
+    const editAddressForm = document.querySelector(`#EditAddress_${addressId}`);
+    this.previouslyFocusedElement = document.activeElement; 
+    
+    editAddressForm.classList.add("active");
+
+    const cancelButton = editAddressForm.querySelector(selectors.cancelEditAddressButton);
+
+    const formContainer = editAddressForm;
+    formContainer.querySelector('.cancel-button-edit-form.hidden').remove();
+
+    if (cancelButton && formContainer) {
+      trapFocus(formContainer , cancelButton);
+    } 
+  }
 }
